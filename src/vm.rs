544237c0e700a3506eeace5e8f493470f1a6dd06
David L. Inman <davidlevere@gmail.com>
2026-08-08 16:55:54 -0400

    add ops

--- src/vm.rs
+++ src/vm.rs
@@ -53,6 +53,77 @@ impl Vm {
             }
 
             match self.procs[self.current.id].instrs[self.current.ip] {
+                CompiledOp::Address(Dest, Reg, isize) => {
+
+                }, 
+                CompiledOp::ProcAddress(Dest, usize => {
+
+                }),
+                CompiledOp::Mov(NumType, Dest, Src) => {
+
+                },
+                CompiledOp::Jump(usize) => {
+
+                },
+                CompiledOp::Bnz(usize, Src) => {
+                },
+                CompiledOp::Bz(usize, Src) => {
+
+                },
+                CompiledOp::AllocateData { dest: Dest, size: Src, align: Src } => {
+
+                },
+                CompiledOp::Call(usize, Vec<Src>) => {
+
+                },
+                CompiledOp::DynCall(Src, Vec<Src>) => {
+
+                },
+                CompiledOp::SysCall(usize, Vec<Src>) => {
+
+                },
+                CompiledOp::Add(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Sub(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Mul(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Div(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Exp(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Mod(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Neg(NumType, Dest, Src) => { 
+
+                },
+                CompiledOp::Eq(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Gt(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Lt(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::And(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Or(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Xor(NumType, Dest, Src, Src) => {
+
+                },
+                CompiledOp::Not(NumType, Dest, Src) => {
+
+                },
                 /*
                 Op::Jump(label) => {
                     self.current.ip = label;
